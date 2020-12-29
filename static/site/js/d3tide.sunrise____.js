b21bf044a5d7c10a374a2da2d1180660daf7839b
takamitsu-iida <takamitsu.iida@gmail.com>
2020-12-30 00:21:12 +0900

    create 2021 js

--- static/site/js/d3tide.sunrise____.js
+++ static/site/js/d3tide.sunrise____.js
@@ -7,8 +7,24 @@
 // 国立天文台 各地のこよみ
 // 日の出・日の入
 // http://eco.mtk.nao.ac.jp/koyomi/dni/
-// 
+//
 // このサイトでは月単位でしか表示されないので、画面をコピペして一年分にする。
+
+// 1ヶ月ごとウェブ画面をコピペしてemacsで加工して作成
+// emacsにコピペ
+// meta-x replace-regexp
+// ^
+// 2020/01
+// 日付の1〜9を01〜09に書き換える
+// meta-x kill-rectangle
+// meta-x replace-string でスペースを,に
+// meta-x replace-regexp
+// ^
+// '
+// meta-x replace-regexp
+// $
+// ',
+// 最終行の,を削除
 
 // 20XX年
 
